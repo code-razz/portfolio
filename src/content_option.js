0254991a518f91ef027bfec3112614e35e6afb0c
Raj Sah <rajsah5557@gmail.com>
2024-05-04 19:37:20 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,171 @@
+const logotext = "RAJ";
+const meta = {
+    title: "Raj Sah",
+    description: "I’m Raj Sah a student of CSE(IoT and Cyber Security including Blockchain Technology)",
+};
+
+const introdata = {
+    title: "I’m Raj Sah",
+    animated: {
+        first: "I love coding",
+        second: "I love travelling",
+        third: "I  love games",
+    },
+    description: "I a CSE student specializing in IoT and Cyber Security including Blockchain Technology at BMSCE",
+    your_img_url: "https://firebasestorage.googleapis.com/v0/b/nivesha-759e3.appspot.com/o/file.png?alt=media&token=a326a59a-e481-42b4-8150-99e2fcc1dc2e",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "C++",
+        value: 75,
+    },
+    {
+        name: "Java",
+        value: 55,
+    },
+    {
+        name: "C",
+        value: 70,
+    },
+    {
+        name: "Javascript",
+        value: 50,
+    },
+    {
+        name: "React",
+        value: 40,
+    },
+    {
+        name: "Node js",
+        value: 50,
+    },
+    
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "rajsah5556@gmail.com",
+    YOUR_FONE: "+91 74829 78787",
+    description: "",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/code-razz",
+    facebook: "https://www.facebook.com/r4raj/",
+    linkedin: "https://www.linkedin.com/in/rajkrsah",
+    twitter: "https://twitter.com/beyond_rule",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
